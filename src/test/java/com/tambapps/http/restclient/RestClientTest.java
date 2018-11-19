7db58f1c7a98df894e2a6e35855e7bf8b0d0ce6c
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-11-19 17:21:05 +0100

    added tests for async requests

--- src/test/java/com/tambapps/http/restclient/RestClientTest.java
+++ src/test/java/com/tambapps/http/restclient/RestClientTest.java
@@ -4,9 +4,11 @@ import com.google.gson.Gson;
 import com.tambapps.http.restclient.data.Post;
 import com.tambapps.http.restclient.request.RestRequest;
 import com.tambapps.http.restclient.request.handler.output.BodyHandlers;
+import com.tambapps.http.restclient.request.handler.response.ResponseHandler;
 import com.tambapps.http.restclient.request.handler.response.ResponseHandlers;
 import com.tambapps.http.restclient.response.RestResponse;
 import com.tambapps.http.restclient.util.ObjectParser;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,9 +16,13 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class RestClientTest {
 
   private static final String API_URL = "https://jsonplaceholder.typicode.com/";
+  private static final int TIMEOUT = 8;
   private static final Gson GSON = new Gson();
   private static final ObjectParser JSON_PARSER = new ObjectParser() {
     @Override
@@ -24,19 +30,38 @@ public class RestClientTest {
       return GSON.fromJson(data, clazz);
     }
   };
+  private static final ResponseHandler<Post> RESPONSE_HANDLER =
+      ResponseHandlers.objectHandler(Post.class, JSON_PARSER);
 
   private final RestClient client = new RestClient(API_URL, 1);
+  private CountDownLatch latch; //allows to wait until async code is executed
+
+  @Before
+  public void init() {
+    latch = new CountDownLatch(1);
+  }
 
   @Test
-  public void getTest() {
+  public void getTest() throws InterruptedException {
     RestRequest request = RestRequest.builder("posts/1")
         .GET()
         .build();
-    RestResponse<Post, ?> response = client.execute(request,
-        ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
+    client.executeAsync(request, RESPONSE_HANDLER, new RestClient.Callback<Post, Post>() {
+      @Override
+      public void call(RestResponse<Post, Post> response) {
+        getAsserts(response);
+        latch.countDown();
+      }
+    });
 
+    RestResponse<Post, ?> response = client.execute(request, RESPONSE_HANDLER);
+    getAsserts(response);
+    assertTrue("Should be true", latch.await(TIMEOUT, TimeUnit.SECONDS));
+  }
+
+  private void getAsserts(RestResponse<Post, ?> response) {
     assertTrue("Should be successful", response.isSuccessful());
-    assertFalse("Shouldn't be null", response.isErrorResponse());
+    assertFalse("Shouldn't be an error code", response.isErrorResponse());
 
     Post post = response.getSuccessData();
     assertNotNull("Shouldn't be null", post);
@@ -47,36 +72,56 @@ public class RestClientTest {
   }
 
   @Test
-  public void putTest() {
+  public void putTest() throws InterruptedException {
     int id = 8;
-    Post post = new Post(id, 43, "title", "body");
+    final Post post = new Post(id, 43, "title", "body");
     RestRequest request = RestRequest.builder("posts/" + id)
         .PUT()
         .output(BodyHandlers.json(GSON.toJson(post)))
         .build();
 
-    RestResponse<Post, ?> response = client.execute(request,
-        ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
+    client.executeAsync(request, RESPONSE_HANDLER, new RestClient.Callback<Post, Post>() {
+      @Override
+      public void call(RestResponse<Post, Post> response) {
+        putAsserts(response, post);
+        latch.countDown();
+      }
+    });
 
-    assertFalse("Shouldn't be null", response.isErrorResponse());
+    RestResponse<Post, ?> response = client.execute(request, RESPONSE_HANDLER);
+    putAsserts(response, post);
+    assertTrue("Should be true", latch.await(TIMEOUT, TimeUnit.SECONDS));
+  }
+
+  private void putAsserts(RestResponse<Post, ?> response, Post post) {
+    assertFalse("Shouldn't be an error code", response.isErrorResponse());
     assertTrue("Should be successful", response.isSuccessful());
     assertEquals("Should be equal", post, response.getSuccessData());
   }
 
   @Test
   public void postTest() {
-    Post post = new Post(0, 43, "title", "body");
+    final Post post = new Post(0, 43, "title", "body");
     RestRequest request = RestRequest.builder("posts/")
         .POST()
         .output(BodyHandlers.json(GSON.toJson(post)))
         .build();
 
-    RestResponse<Post, ?> response = client.execute(request,
-        ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
+    client.executeAsync(request, RESPONSE_HANDLER, new RestClient.Callback<Post, Post>() {
+      @Override
+      public void call(RestResponse<Post, Post> response) {
+        postAsserts(response, post);
+        latch.countDown();
+      }
+    });
 
+    RestResponse<Post, ?> response = client.execute(request, RESPONSE_HANDLER);
+    postAsserts(response, post);
+  }
+
+  private void postAsserts(RestResponse<Post, ?> response, Post post) {
     assertTrue("Should be successful", response.isSuccessful());
     assertFalse("Shouldn't be null", response.isErrorResponse());
-
     Post responseData = response.getSuccessData();
     assertEquals("Should be equal", post.getUserId(), responseData.getUserId());
     assertEquals("Should be equal", post.getTitle(), responseData.getTitle());
@@ -85,13 +130,48 @@ public class RestClientTest {
 
   @Test
   public void deleteTest() {
-    RestRequest request = RestRequest.builder("posts/1")
+    RestRequest request = RestRequest.builder("posts/2")
         .DELETE()
         .build();
-    RestResponse<Post, ?> response = client.execute(request,
-        ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
-
-    assertTrue("Should be successful", response.isSuccessful());
-    assertFalse("Shouldn't be null", response.isErrorResponse());
+    client.executeAsync(request, RESPONSE_HANDLER, new RestClient.Callback<Post, Post>() {
+      @Override
+      public void call(RestResponse<Post, Post> response) {
+        deleteAsserts(response);
+        latch.countDown();
+      }
+    });
+    RestResponse<Post, ?> response = client.execute(RestRequest.builder("posts/1")
+        .DELETE()
+        .build(), RESPONSE_HANDLER);
+    deleteAsserts(response);
   }
+
+  private void deleteAsserts(RestResponse<Post, ?> response) {
+    assertTrue("Should be successful", response.isSuccessful());
+    assertFalse("Shouldn't be an error code", response.isErrorResponse());
+  }
+
+  @Test
+  public void deleteNotFoundTest() {
+    RestRequest request = RestRequest.builder("posts/1456846854665")
+        .DELETE()
+        .build();
+    client.executeAsync(request, RESPONSE_HANDLER, new RestClient.Callback<Post, Post>() {
+      @Override
+      public void call(RestResponse<Post, Post> response) {
+        deleteNotFoundAsserts(response);
+        latch.countDown();
+      }
+    });
+    RestResponse<Post, ?> response = client.execute(request, RESPONSE_HANDLER);
+
+    deleteNotFoundAsserts(response);
+  }
+
+  private void deleteNotFoundAsserts(RestResponse<Post, ?> response) {
+    assertTrue("Shouldn't be successful", response.isSuccessful());
+    assertTrue("Should be an error code", response.isErrorResponse());
+    assertEquals("Should be equal", 404, response.getResponseCode());
+  }
+
 }
