d1dd32974f24851bd824181ad771fa87702f263d
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-11-19 16:39:32 +0100

    continued implement unit tests

--- src/test/java/com/tambapps/http/restclient/RestClientTest.java
+++ src/test/java/com/tambapps/http/restclient/RestClientTest.java
@@ -3,39 +3,40 @@ package com.tambapps.http.restclient;
 import com.google.gson.Gson;
 import com.tambapps.http.restclient.data.Post;
 import com.tambapps.http.restclient.request.RestRequest;
+import com.tambapps.http.restclient.request.handler.output.BodyHandlers;
 import com.tambapps.http.restclient.request.handler.response.ResponseHandlers;
 import com.tambapps.http.restclient.response.RestResponse;
 import com.tambapps.http.restclient.util.ObjectParser;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class RestClientTest {
 
   private static final String API_URL = "https://jsonplaceholder.typicode.com/";
+  private static final Gson GSON = new Gson();
   private static final ObjectParser JSON_PARSER = new ObjectParser() {
-    final Gson gson = new Gson();
     @Override
     public <T> T parse(Class<T> clazz, String data) {
-      return gson.fromJson(data, clazz);
+      return GSON.fromJson(data, clazz);
     }
   };
 
   private final RestClient client = new RestClient(API_URL, 1);
 
-
   @Test
   public void getTest() {
-    RestRequest request = RestRequest.builder()
+    RestRequest request = RestRequest.builder("posts/1")
         .GET()
-        .endpoint("posts/1")
         .build();
     RestResponse<Post, ?> response = client.execute(request,
         ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
 
     assertTrue("Should be successful", response.isSuccessful());
+    assertFalse("Shouldn't be null", response.isErrorResponse());
 
     Post post = response.getSuccessData();
     assertNotNull("Shouldn't be null", post);
@@ -45,5 +46,52 @@ public class RestClientTest {
     assertNotNull("Shouldn't be null", post.getTitle());
   }
 
+  @Test
+  public void putTest() {
+    int id = 8;
+    Post post = new Post(id, 43, "title", "body");
+    RestRequest request = RestRequest.builder("posts/" + id)
+        .PUT()
+        .output(BodyHandlers.json(GSON.toJson(post)))
+        .build();
 
+    RestResponse<Post, ?> response = client.execute(request,
+        ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
+
+    assertFalse("Shouldn't be null", response.isErrorResponse());
+    assertTrue("Should be successful", response.isSuccessful());
+    assertEquals("Should be equal", post, response.getSuccessData());
+  }
+
+  @Test
+  public void postTest() {
+    Post post = new Post(0, 43, "title", "body");
+    RestRequest request = RestRequest.builder("posts/")
+        .POST()
+        .output(BodyHandlers.json(GSON.toJson(post)))
+        .build();
+
+    RestResponse<Post, ?> response = client.execute(request,
+        ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
+
+    assertTrue("Should be successful", response.isSuccessful());
+    assertFalse("Shouldn't be null", response.isErrorResponse());
+
+    Post responseData = response.getSuccessData();
+    assertEquals("Should be equal", post.getUserId(), responseData.getUserId());
+    assertEquals("Should be equal", post.getTitle(), responseData.getTitle());
+    assertEquals("Should be equal", post.getBody(), responseData.getBody());
+  }
+
+  @Test
+  public void deleteTest() {
+    RestRequest request = RestRequest.builder("posts/1")
+        .DELETE()
+        .build();
+    RestResponse<Post, ?> response = client.execute(request,
+        ResponseHandlers.objectHandler(Post.class, JSON_PARSER));
+
+    assertTrue("Should be successful", response.isSuccessful());
+    assertFalse("Shouldn't be null", response.isErrorResponse());
+  }
 }
--- src/test/java/com/tambapps/http/restclient/data/Post.java
+++ src/test/java/com/tambapps/http/restclient/data/Post.java
@@ -1,11 +1,20 @@
 package com.tambapps.http.restclient.data;
 
+import java.util.Objects;
+
 public class Post {
 
   private Integer id;
   private Integer userId;
   private String title;
   private String body;
+
+  public Post(Integer id, Integer userId, String title, String body) {
+    this.id = id;
+    this.userId = userId;
+    this.title = title;
+    this.body = body;
+  }
 
   public Integer getId() {
     return id;
@@ -21,5 +30,34 @@ public class Post {
 
   public String getBody() {
     return body;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Post post = (Post) o;
+    return Objects.equals(id, post.id) &&
+        Objects.equals(userId, post.userId) &&
+        Objects.equals(title, post.title) &&
+        Objects.equals(body, post.body);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override public String toString() {
+    return "Post{" +
+        "id=" + id +
+        ", userId=" + userId +
+        ", title='" + title + '\'' +
+        ", body='" + body + '\'' +
+        '}';
   }
 }
